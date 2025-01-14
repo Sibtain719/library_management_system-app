ef9f4f23e59935651be1a66a59361c4f165b6163
Sibtain719 <sibtainali896@gmail.com>
2025-01-14 13:27:00 +0500

    admin functions done

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,5 +6,58 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    function addBooks(Request $request)
+    {
+
+    }
+
+    function addMembers(Request $request)
+    {
+
+    }
+
+    function deleteBooks(Request $request)
+    {
+
+    }
+    function editBooks(Request $request)
+    {
+
+    }
+
+
+    function deleteMembers(Request $request)
+    {
+
+    }
+
+    function transactions(Request $request)
+    {
+
+    }
+
+    function addNewTransaction(Request $request)
+    {
+
+    }
+
+    function deleteTransactions(Request $request)
+    {
+
+    }
+
+    function editTransactions(Request $request)
+    {
+
+    }
+
+    function viewReports(Request $request)
+    {
+
+    }
+
+    function generateRreports(Request $request)
+    {
+
+    }
 }
